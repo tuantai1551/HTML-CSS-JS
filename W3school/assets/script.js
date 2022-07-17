f062470813bc2a6490f178e499c94523cebd4057
tai <2054054054tai@ou.edu.vn>
2022-07-17 07:26:46 +0700

    HTML-CSS-JS

--- W3school/assets/script.js
+++ W3school/assets/script.js
@@ -1,23 +1,39 @@
-const btnBuyTickets = document.querySelectorAll(".js-buy-ticket");
-const modal = document.querySelector(".modal");
-const ModalClose = document.querySelector(".modal-close");
-const FooterClose = document.querySelector(".footer-modal-close");
+// // Mua vé
+// const btnBuyTickets = document.querySelectorAll(".js-buy-ticket");
+// const modal = document.querySelector(".modal");
+// const ModalClose = document.querySelector(".modal-close");
+// const FooterClose = document.querySelector(".footer-modal-close");
 
-function showModal() {
-  open.classList.add("modal");
-}
+// function showModal() {
+//   open.classList.add("modal");
+// }
 
-function closeModal() {
-  open.classList.remove("modal");
-}
-for (const btnBuyTicket of btnBuyTickets) {
-  btnBuyTicket.addEventListener("click", showModal);
-}
+// function closeModal() {
+//   open.classList.remove("modal");
+// }
+// for (const btnBuyTicket of btnBuyTickets) {
+//   btnBuyTicket.addEventListener("click", showModal);
+// }
 
-for (const btnBuyTicket of btnBuyTickets) {
-    ModalClose.addEventListener("click", closeModal);
-}
-for (const btnBuyTicket of btnBuyTickets) {
-    FooterClose.addEventListener("click", closeModal);
-}
+// for (const btnBuyTicket of btnBuyTickets) {
+//   ModalClose.addEventListener("click", closeModal);
+// }
+// for (const btnBuyTicket of btnBuyTickets) {
+//   FooterClose.addEventListener("click", closeModal);
+// }
 
+// Menu của tablet
+const btnMenuTable = document.querySelector(".mobile-menu-btn");
+const headerMenu = document.getElementById("header");
+
+var headerHeight = header.clientHeight;
+btnMenuTable.addEventListener("click", function () {
+  var a = headerMenu.clientHeight == headerHeight;
+  if(a){
+    headerMenu.style.height = "auto"
+  }
+  else
+  {
+    headerMenu.style.height = null;
+  }
+});
